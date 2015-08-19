6daff887bdda580acc718d5aecb453f1f9f9a65f
heinzec <heinzec@a1c88af4-34b9-405c-8f8d-2e728b2986e8>
2015-08-19 11:59:52 +0000

    resolve conflict
    git-svn-id: https://code.vis.ethz.ch/svn/nicausalai/packages/backShift@783 a1c88af4-34b9-405c-8f8d-2e728b2986e8

--- R/checkDiagonalization.R
+++ R/checkDiagonalization.R
@@ -17,55 +17,7 @@ plotDiagonalization <- function(estConnectivity, X, env, whichEnv){
   tmp <- (diag(p) - t(estConnectivity))%*%deltas[[whichEnv]]%*%t(diag(p) - t(estConnectivity))
   deltaIntVar <- abs(tmp)/max(abs(tmp))
   
-  image(
-    z  = deltaIntVar,
-    axes = FALSE, 
-    col = fields::tim.colors(),
-    main = paste("Env.", whichEnv))
-  axis(
-    side     = 1,
-    labels   = 1:p,
-    at       = seq( 0, 1, length = p ),
-    cex.axis = 0.85)
-  axis(
-    side     = 2,
-    labels   = 1:p,
-    at       = seq( 0, 1, length = p ),
-    cex.axis = 0.85)
-}
+  fields::image.plot(z = deltaIntVar, col=fields::tim.colors(), legend.shrink = 0.8,
+                     main = paste("Env.", whichEnv))
 
-plotDiagonalizationAll <- function(estConnectivity, X, env){
-  deltas <- computeDelta(X, env)$Delta
-  p <- ncol(estConnectivity)
-  K <- length(deltas)
-  
-  deltaIntVar <- vector("list", K)
-  
-  for(i in 1:K){
-    tmp <- (diag(p) - t(estConnectivity))%*%deltas[[i]]%*%t(diag(p) - t(estConnectivity))
-    deltaIntVar[[i]] <- abs(tmp)/max(abs(tmp))
-  }
-  
-  gridSize <- ceiling(sqrt(K))
-  
-  par(mfrow = c(gridSize, gridSize))
-  for(i in 1:K){
-    image(
-      z  = deltaIntVar[[i]],
-      axes = FALSE, 
-      col = fields::tim.colors(),
-      main = paste("Environment", i))
-    axis(
-      side     = 1,
-      labels   = 1:p,
-      at       = seq( 0, 1, length = p ),
-      cex.axis = 0.85)
-    axis(
-      side     = 2,
-      labels   = 1:p,
-      at       = seq( 0, 1, length = p ),
-      cex.axis = 0.85)
-    fields::image.plot(1:p, 1:p, deltaIntVar[[i]], legend.only=TRUE, col=fields::tim.colors())
-  }
-  
 }
