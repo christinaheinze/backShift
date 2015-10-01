e0fdaadedc753b73f5ef928ba972ecca8c56289d
heinzec <heinzec@a1c88af4-34b9-405c-8f8d-2e728b2986e8>
2015-10-01 13:52:52 +0000

    bug fix in plotting function, added title parameter in plotDiagonalization()
    git-svn-id: https://code.vis.ethz.ch/svn/nicausalai/packages/backShift@813 a1c88af4-34b9-405c-8f8d-2e728b2986e8

--- R/checkDiagonalization.R
+++ R/checkDiagonalization.R
@@ -6,18 +6,21 @@
 #' @param env Indicator of the experiment or the intervention type an 
 #' observation belongs to (a numeric vector of length n).
 #' @param whichEnv Indicator for the environment to be plotted.
+#' @param Optional title for plot; defaults to paste("Env.", whichEnv) 
 #'  
-plotDiagonalization <- function(estConnectivity, X, env, whichEnv){
+plotDiagonalization <- function(estConnectivity, X, env, whichEnv, main = NULL){
   deltas <- computeDelta(X, env)$Delta
   p <- ncol(estConnectivity)
   K <- length(deltas)
-  
+  settings <- sort(unique(env))
   deltaIntVar <- vector("list", K)
   
-  tmp <- (diag(p) - t(estConnectivity))%*%deltas[[whichEnv]]%*%t(diag(p) - t(estConnectivity))
+  tmp <- (diag(p) - t(estConnectivity))%*%deltas[[which(settings == whichEnv)]]%*%t(diag(p) - t(estConnectivity))
   deltaIntVar <- abs(tmp)/max(abs(tmp))
   
+  if(is.null(main)) paste("Env.", whichEnv) else main
+  
   fields::image.plot(z = deltaIntVar, col=fields::tim.colors(), legend.shrink = 0.8,
-                     main = paste("Env.", whichEnv))
+                     main = main)
 
 }
